68d11814d9726d9447451042023cf02704038c81
Oleh Kovalchyn <jobkovalchyn@gmail.com>
2019-12-11 18:04:40 +0200

    add EBS

--- main.tf
+++ main.tf
@@ -7,12 +7,10 @@ provider "aws" {
 //////////EC2/////////////
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0233214e13e500f77"  ///////instance ami
+  ami                    = "ami-0233214e13e500f77"  ///////instance ami NEED TO CHANGE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sec_group.id]
-  user_data              = <<EOF
-
-EOF
+  user_data              = file("script.sh")
 }
 
 
@@ -42,6 +40,18 @@ resource "aws_security_group" "sec_group" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+////////////EBS////////////////
+
+resource "aws_ebs_volume" "ebs" {
+  availability_zone = "us-west-2"
+  size              = 10
+  /// type = find free !!!!!!!!!!!!!!!!!
+
+  tags = {
+    Name = "EBS 10G FOR EC2"
   }
 }
 
